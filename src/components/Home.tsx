a116f575e72da2807e2298a8d6e4854bdceec390
Tjcarroll1 <tjcarroll1@icloud.com>
2023-07-12 14:30:10 +1000

    added link to menu button

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,40 +3,40 @@ import { Box } from "@mui/system";
 import React from "react";
 import verticalSVG from "../assets/vertical-hello.svg";
 import horizontal from "../assets/horizontal.svg";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  const smallScreen = useMediaQuery("(min-width: 300px) and (max-width: 600px)");
-  const mediumScreen = useMediaQuery("(min-width: 600px) and (max-width: 1350px)");
+  const smallScreen = useMediaQuery(
+    "(min-width: 300px) and (max-width: 600px)"
+  );
+  const mediumScreen = useMediaQuery(
+    "(min-width: 600px) and (max-width: 1350px)"
+  );
 
   let bgSVG;
   let headingSize;
   let buttonPos;
   let SVGsize;
 
-  
   if (smallScreen) {
     bgSVG = "url(" + verticalSVG + ")";
     headingSize = "6rem";
     buttonPos = -200;
     SVGsize = "70%";
-   
   } else if (mediumScreen) {
     bgSVG = "url(" + horizontal + ")";
     headingSize = "8rem";
     buttonPos = -200;
     SVGsize = "90%";
-   
   } else {
     bgSVG = "url(" + horizontal + ")";
     headingSize = "12rem";
     buttonPos = -250;
     SVGsize = "95%";
-    
   }
 
   return (
     <div>
-      
       <Box
         sx={{
           backgroundColor: "#5B71EA",
@@ -52,19 +52,23 @@ const Home = () => {
           alignItems: "center",
         }}
       >
-    
+        <Link to="/menu">
           <Button
-            href="./menu"
             sx={{
               width: "150px",
               height: "50px",
               fontSize: 20,
               top: buttonPos,
-            }}>
+            }}
+          >
             Menu
           </Button>
-       
-        <Typography variant="h1" sx={{ color: "#FFFFFF", fontSize: headingSize }}>
+        </Link>
+
+        <Typography
+          variant="h1"
+          sx={{ color: "#FFFFFF", fontSize: headingSize }}
+        >
           Toby J
         </Typography>
         <Typography variant="h3">UX/UI Designer</Typography>
